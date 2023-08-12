ffcd3d619da9adf672d2f998452302a66e29eab3
ohdair <ohdiar@gmail.com>
2023-08-12 18:53:47 +0900

    feat: #2 Separation 추가

---
+++ Sources/GesturableGraph/Separation.swift
@@ -0,0 +1,16 @@
+//
+//  Separation.swift
+//  
+//
+//  Created by 박재우 on 2023/08/12.
+//
+
+import Foundation
+
+// 0 means there is no separation
+// Increases in percentage based on the height of the graph.
+// The default value for top is 30 and bottom is 30.
+struct Separation {
+    var top: Int = 30
+    var bottom: Int = 30
+}
