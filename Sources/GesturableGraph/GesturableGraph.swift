ab965d6ab78c66697ca7beecb77b4fbc82335521
ohdair <ohdiar@gmail.com>
2023-08-18 14:59:05 +0900

    feat: draw 메서드 추가
    - 곡선 그래프를 그리기 위한 UIBezierPath 생성하는 initializer 추가

--- Sources/GesturableGraph/GesturableGraph.swift
+++ Sources/GesturableGraph/GesturableGraph.swift
@@ -19,6 +19,25 @@ public class GesturableGraph: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    public override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        UIColor.blue.setStroke()
+        let points = convertToPoints()
+        let curveLinePath = UIBezierPath(quadCurve: points)
+
+        curveLinePath?.lineWidth = 2
+        curveLinePath?.lineJoinStyle = .round
+        curveLinePath?.lineCapStyle = .round
+        curveLinePath?.stroke()
+
+        points.forEach { point in
+            let pointPath = UIBezierPath(ovalIn: CGRect(x: point.x - 2, y: point.y - 2, width: 4, height: 4))
+            UIColor.gray.set()
+            pointPath.fill()
+        }
+    }
 }
 
 //MARK: - 속성 값을 변경하는 메서드
@@ -62,6 +81,50 @@ extension GesturableGraph {
         }
 
         return bounds.height / (calibrationTop - calibrationBottom) * (calibrationTop - element)
+    }
+}
+
+//MARK: - UIBezierPath 생성하는
+public extension UIBezierPath {
+    convenience init?(quadCurve points: [CGPoint]) {
+        guard points.count > 1 else {
+            return nil
+        }
+
+        self.init()
+
+        // 시작점 표시
+        var p0 = points[0]
+        move(to: p0)
+
+        // 값이 2개라면 직선으로 표시
+        guard points.count != 2 else {
+            addLine(to: points[1])
+            return
+        }
+
+        // 중간점을 잡고 2개의 곡선 만들기
+        for i in 1..<points.count {
+            let p2 = midPoint(from: p0, to: points[i])
+
+            addQuadCurve(to: p2, controlPoint: controlPoint(p0: p0, p2: p2))
+            addQuadCurve(to: points[i], controlPoint: controlPoint(p0: points[i], p2: p2))
+
+            p0 = points[i]
+        }
+    }
+
+    private func midPoint(from p1: CGPoint, to p2: CGPoint) -> CGPoint {
+        return CGPoint(x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2)
+    }
+
+    // 상향곡선으로 그릴 때에는 p0에 목적지로 두어야 하고,
+    // 하향곡선에서는 p2에 목적지를 두어야 한다
+    private func controlPoint(p0: CGPoint, p2: CGPoint) -> CGPoint {
+        var p1 = midPoint(from: p0, to: p2)
+        p1.y = p0.y
+
+        return p1
     }
 }
 
