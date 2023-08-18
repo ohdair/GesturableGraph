d811c15c99aedcd3b54a106216d698ec69d8294d
ohdair <ohdiar@gmail.com>
2023-08-18 14:11:54 +0900

    feat: GesturableGraph 추가
    - initializer 추가
    - padding 변경하는 메서드 추가

--- Sources/GesturableGraph/GesturableGraph.swift
+++ Sources/GesturableGraph/GesturableGraph.swift
@@ -1,6 +1,30 @@
-public struct GesturableGraph {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+public class GesturableGraph: UIView {
+    private var verticalPadding: Separation
+    let elements: [Double]
+    var distribution: Distribution
+
+    public init(_ frame: CGRect = .zero, elements: [Double]) {
+        self.elements = elements
+        self.distribution = .equalSpacing
+        self.verticalPadding = Separation()
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension GesturableGraph {
+    func padding(top: Int) {
+        guard top >= 0 else { return }
+        verticalPadding.top = top
+    }
+
+    func padding(bottom: Int) {
+        guard bottom >= 0 else { return }
+        verticalPadding.bottom = bottom
     }
 }
