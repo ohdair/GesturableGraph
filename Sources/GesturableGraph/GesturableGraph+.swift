0c1357187966a652b07c22cadb5a72065c2ebfa5
ohdair <ohdiar@gmail.com>
2023-09-05 17:27:37 +0900

    refactor: graph으로 넘어간 속성 분리
    - 기본값 추가

--- Sources/GesturableGraph/GesturableGraph+.swift
+++ Sources/GesturableGraph/GesturableGraph+.swift
@@ -1,52 +1,25 @@
 import UIKit
 
 extension GesturableGraph {
-    // 0 means there is no separation
-    // Increases in percentage based on the height of the graph.
-    // The default value for top is 30 and bottom is 30.
-    struct VerticalPadding {
-        var top: Double
-        var bottom: Double
-    }
-
-    public enum GraphType {
-        case curve
-        case straight
-    }
-
-    // equalSpacing is [o..o..o..o]
-    // aroundSpacing is [.o..o..o..o.]
-    // evenSpacing is [..o..o..o..o..]
-    public enum Distribution: Int {
-        case equalSpacing = -1
-        case aroundSpacing
-        case evenSpacing
-    }
-
     public struct GraphLine {
-        public var width: Double
-        public var color: UIColor
+        public var width: Double = Constraints.lineWidth
+        public var color: UIColor = Constraints.lineColor
     }
 
     public struct GraphPoint {
-        public var width: Double
-        public var color: UIColor
-        public var isHidden: Bool
+        public var width: Double = Constraints.pointWidth
+        public var color: UIColor = Constraints.pointColor
+        public var isHidden: Bool = Constraints.pointIsHidden
     }
 
     public struct GraphArea {
-        var _colors: [UIColor] = []
-        public var colors: [UIColor] {
-            get {
-                return _colors
-            }
-            set {
-                if newValue.isEmpty {
-                    return
+        public var colors: [UIColor] = Constraints.areaColors {
+            didSet {
+                if colors.isEmpty {
+                    colors = oldValue
                 }
-                _colors = newValue
             }
         }
-        public var isFill: Bool
+        public var isFill: Bool = Constraints.areaIsFill
     }
 }
