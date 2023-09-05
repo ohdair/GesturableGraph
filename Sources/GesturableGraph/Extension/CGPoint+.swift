5a310e790ee6a5a23dbed063940d46644de53bfa
ohdair <ohdiar@gmail.com>
2023-09-05 11:22:40 +0900

    refactor: CGPoint 관련 메서드 extension으로 분리

---
+++ Sources/GesturableGraph/Extension/CGPoint+.swift
@@ -0,0 +1,18 @@
+//
+//  CGPoint+.swift
+//
+//
+//  Created by 박재우 on 9/5/23.
+//
+
+import Foundation
+
+extension CGPoint {
+    func midPoint(from point: CGPoint) -> CGPoint {
+        return CGPoint(x: (self.x + point.x) / 2, y: (self.y + point.y) / 2)
+    }
+
+    func controlPoint(from point: CGPoint) -> CGPoint {
+        return CGPoint(x: midPoint(from: point).x, y: self.x)
+    }
+}
--- Sources/GesturableGraph/Extension/UIBezierPath+.swift
+++ Sources/GesturableGraph/Extension/UIBezierPath+.swift
@@ -36,10 +36,10 @@ extension UIBezierPath {
         move(to: p0)
 
         for i in 1..<points.count {
-            let p2 = midPoint(from: p0, to: points[i])
+            let p2 = points[i].midPoint(from: p0)
 
-            addQuadCurve(to: p2, controlPoint: controlPoint(p0: p0, p2: p2))
-            addQuadCurve(to: points[i], controlPoint: controlPoint(p0: points[i], p2: p2))
+            addQuadCurve(to: p2, controlPoint: p2.controlPoint(from: p0))
+            addQuadCurve(to: points[i], controlPoint: points[i].controlPoint(from: p2))
 
             p0 = points[i]
         }
@@ -53,7 +53,7 @@ extension UIBezierPath {
             return nil
         }
 
-        let midPoint = midPoint(from: points[firstIndex], to: points[firstIndex + 1])
+        let midPoint = points[firstIndex + 1].midPoint(from: points[firstIndex])
         let p2 = midPoint
         let p0 = pointX > midPoint.x ? points[firstIndex + 1] : points[firstIndex]
 
@@ -75,19 +75,8 @@ extension UIBezierPath {
     }
 
     private func cacultatedY(pointX: Double, p0: CGPoint, p2: CGPoint) -> Double {
-        let p1 = controlPoint(p0: p0, p2: p2)
+        let p1 = p2.controlPoint(from: p0)
         let t = (pointX - p0.x) / (p2.x - p0.x)
         return pow(1 - t, 2) * p0.y + 2 * (1 - t) * t * p1.y + pow(t, 2) * p2.y
-    }
-
-    private func midPoint(from p1: CGPoint, to p2: CGPoint) -> CGPoint {
-        return CGPoint(x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2)
-    }
-
-    private func controlPoint(p0: CGPoint, p2: CGPoint) -> CGPoint {
-        var p1 = midPoint(from: p0, to: p2)
-        p1.y = p0.y
-
-        return p1
     }
 }
