5bde0796afcd6cf68a52f28c77a1ce2efd8dbf87
ohdair <ohdiar@gmail.com>
2023-09-12 16:27:56 +0900

    test: 모델 분리로 인한 Tests 수정

--- Tests/GesturableGraphTests/GesturableGraphTests.swift
+++ Tests/GesturableGraphTests/GesturableGraphTests.swift
@@ -86,142 +86,154 @@ final class GesturableGraphTests: XCTestCase {
     }
 
     //MARK: - convertToPoints Test
-    // elements에 맞게 설정된 GesturableGraph
-    @MainActor
-    private func gesturableGraphForConvertToPoints(elements: [Double]) -> GesturableGraph? {
-        let gesturableGraph = GesturableGraph(elements: elements)
-        gesturableGraph?.paddingOfTop(scale: 0)
-        gesturableGraph?.paddingOfBottom(scale: 0)
-        GesturableGraphConstraint.graphWidth = CGFloat(elements.count - 1) * 100
-        GesturableGraphConstraint.graphHeight = ((elements.max() ?? 0) - (elements.min() ?? 0)) * 100
+    // elements에 맞게 설정된 graph
+    private func graphForTest(elements: [Double]) -> Graph? {
+        var graph = Graph(elements: elements)
+        graph?.padding.top = 0
+        graph?.padding.bottom = 0
+        graph?.distribution = .equalSpacing
 
-        return gesturableGraph
+        return graph
     }
 
-    @MainActor
     func test_convertToPoints_요소가_양수일_때_값을_제대로_나오는지_확인() {
         let elements: [Double] = [2, 3, 4, 6, 8, 5, 1]
-        guard let gesturableGraph = gesturableGraphForConvertToPoints(elements: elements) else {
-            XCTFail("Failed initialzied GesturableGraph")
+        let width: Double = Double(elements.count - 1) * 100
+        let height: Double = (elements.max()! - elements.min()!) * 100
+        guard let graph = graphForTest(elements: elements) else {
+            XCTFail("Failed initialzied graph")
             return
         }
 
-        let result = gesturableGraph.convertToPoints(elements)
-        let expected = [(0, 600), (100, 500), (200, 400), (300, 200), (400, 0), (500, 300), (600, 700)]
-            .map { CGPoint(x: $0.0, y: $0.1) }
+        let result = graph.points
+        let expected: [Graph.Point] = [(0, 600), (100, 500), (200, 400), (300, 200), (400, 0), (500, 300), (600, 700)]
+            .map { (x: $0.0 / width, y: $0.1 / height) }
 
-        XCTAssertEqual(result, expected)
+        for (r, e) in zip(result, expected) {
+            XCTAssertEqual(r.x, e.x)
+            XCTAssertEqual(r.y, e.y)
+        }
     }
 
-    @MainActor
     func test_convertToPoints_요소가_음수일_때_값을_제대로_나오는지_확인() {
         let elements: [Double] = [-2, -3, -4, -6, -8, -5, -1]
-        guard let gesturableGraph = gesturableGraphForConvertToPoints(elements: elements) else {
-            XCTFail("Failed initialzied GesturableGraph")
+        let width: Double = Double(elements.count - 1) * 100
+        let height: Double = (elements.max()! - elements.min()!) * 100
+        guard let graph = graphForTest(elements: elements) else {
+            XCTFail("Failed initialzied graph")
             return
         }
 
-        let result = gesturableGraph.convertToPoints(elements)
-        let expected = [(0, 100), (100, 200), (200, 300), (300, 500), (400, 700), (500, 400), (600, 0)]
-            .map { CGPoint(x: $0.0, y: $0.1) }
+        let result = graph.points
+        let expected: [Graph.Point] = [(0, 100), (100, 200), (200, 300), (300, 500), (400, 700), (500, 400), (600, 0)]
+            .map { (x: $0.0 / width, y: $0.1 / height) }
 
-        XCTAssertEqual(result, expected)
+        for (r, e) in zip(result, expected) {
+            XCTAssertEqual(r.x, e.x)
+            XCTAssertEqual(r.y, e.y)
+        }
     }
 
-    @MainActor
     func test_convertToPoints_요소가_양수와음수_혼합이어도_제대로_나오는지_확인() {
         let elements: [Double] = [0, -3, 3, -3, 7, 0, 0]
-        guard let gesturableGraph = gesturableGraphForConvertToPoints(elements: elements) else {
-            XCTFail("Failed initialzied GesturableGraph")
+        let width: Double = Double(elements.count - 1) * 100
+        let height: Double = (elements.max()! - elements.min()!) * 100
+        guard let graph = graphForTest(elements: elements) else {
+            XCTFail("Failed initialzied graph")
             return
         }
 
-        let result = gesturableGraph.convertToPoints(elements)
-        let expected = [(0, 700), (100, 1000), (200, 400), (300, 1000), (400, 0), (500, 700), (600, 700)]
-            .map { CGPoint(x: $0.0, y: $0.1) }
+        let result = graph.points
+        let expected: [Graph.Point] = [(0, 700), (100, 1000), (200, 400), (300, 1000), (400, 0), (500, 700), (600, 700)]
+            .map { (x: $0.0 / width, y: $0.1 / height) }
 
-        XCTAssertEqual(result, expected)
-    }
-
-    @MainActor
-    func test_convertToPoints_요소가_1개일_때_값이_nil인지_확인() {
-        let temporaryElements: [Double] = [0, 0, 0]
-        let elements: [Double] = [-2]
-        guard let gesturableGraph = gesturableGraphForConvertToPoints(elements: temporaryElements) else {
-            XCTFail("Failed initialzied GesturableGraph")
-            return
+        for (r, e) in zip(result, expected) {
+            XCTAssertEqual(r.x, e.x)
+            XCTAssertEqual(r.y, e.y)
         }
-
-        let result = gesturableGraph.convertToPoints(elements)
-
-        XCTAssertNil(result)
     }
 
-    @MainActor
     func test_convertToPoints_distributiond이_aroundSpacing일_때_값이_제대로_나오는지_확인() {
         let elements: [Double] = [0, -3, 3, -3, 7]
-        guard let gesturableGraph = gesturableGraphForConvertToPoints(elements: elements) else {
-            XCTFail("Failed initialzied GesturableGraph")
+        let width: Double = Double(elements.count - 1) * 100
+        let height: Double = (elements.max()! - elements.min()!) * 100
+        guard var graph = graphForTest(elements: elements) else {
+            XCTFail("Failed initialzied graph")
             return
         }
 
-        gesturableGraph.distribution = .aroundSpacing
+        graph.distribution = .aroundSpacing
 
-        let result = gesturableGraph.convertToPoints(elements)
-        let expected = [(40, 700), (120, 1000), (200, 400), (280, 1000), (360, 0)]
-            .map { CGPoint(x: $0.0, y: $0.1) }
+        let result = graph.points
+        let expected: [Graph.Point] = [(40, 700), (120, 1000), (200, 400), (280, 1000), (360, 0)]
+            .map { (x: $0.0 / width, y: $0.1 / height) }
 
-        XCTAssertEqual(result, expected)
+        for (r, e) in zip(result, expected) {
+            XCTAssertEqual(r.x, e.x)
+            XCTAssertEqual(r.y, e.y)
+        }
     }
 
-    @MainActor
     func test_convertToPoints_distributiond이_evenSpacing일_때_값이_제대로_나오는지_확인() {
         let elements: [Double] = [0, -3, 3, -3, 7, 1, -1, 3, 7]
-        guard let gesturableGraph = gesturableGraphForConvertToPoints(elements: elements) else {
-            XCTFail("Failed initialzied GesturableGraph")
+        let width: Double = Double(elements.count - 1) * 100
+        let height: Double = (elements.max()! - elements.min()!) * 100
+        guard var graph = graphForTest(elements: elements) else {
+            XCTFail("Failed initialzied graph")
             return
         }
 
-        gesturableGraph.distribution = .evenSpacing
+        graph.distribution = .evenSpacing
 
-        let result = gesturableGraph.convertToPoints(elements)
-        let expected = [(80, 700), (160, 1000), (240, 400), (320, 1000), (400, 0), (480, 600), (560, 800), (640, 400), (720, 0)]
-            .map { CGPoint(x: $0.0, y: $0.1) }
+        let result = graph.points
+        let expected: [Graph.Point] = [(80, 700), (160, 1000), (240, 400), (320, 1000), (400, 0), (480, 600), (560, 800), (640, 400), (720, 0)]
+            .map { (x: $0.0 / width, y: $0.1 / height) }
 
-        XCTAssertEqual(result, expected)
+        for (r, e) in zip(result, expected) {
+            XCTAssertEqual(r.x, e.x)
+            XCTAssertEqual(r.y, e.y)
+        }
     }
 
-    @MainActor
     func test_convertToPoints_verticalPadding의_top이_1일_때_값이_제대로_나오는지_확인() {
         let elements: [Double] = [1, 2, -1, 3]
-        guard let gesturableGraph = gesturableGraphForConvertToPoints(elements: elements) else {
-            XCTFail("Failed initialzied GesturableGraph")
+        let width: Double = Double(elements.count - 1) * 100
+        let height: Double = (elements.max()! - elements.min()!) * 100
+        guard var graph = graphForTest(elements: elements) else {
+            XCTFail("Failed initialzied graph")
             return
         }
 
-        gesturableGraph.paddingOfTop(scale: 1)
+        graph.padding.top = 1
 
-        let result = gesturableGraph.convertToPoints(elements)
-        let expected = [(0, 300), (100, 250), (200, 400), (300, 200)]
-            .map { CGPoint(x: $0.0, y: $0.1) }
+        let result = graph.points
+        let expected: [Graph.Point] = [(0, 300), (100, 250), (200, 400), (300, 200)]
+            .map { (x: $0.0 / width, y: $0.1 / height) }
 
-        XCTAssertEqual(result, expected)
+        for (r, e) in zip(result, expected) {
+            XCTAssertEqual(r.x, e.x)
+            XCTAssertEqual(r.y, e.y)
+        }
     }
 
-    @MainActor
     func test_convertToPoints_verticalPadding의_bottom이_이분의일일_때_값이_제대로_나오는지_확인() {
         let elements: [Double] = [1.25, 2, -1]
-        guard let gesturableGraph = gesturableGraphForConvertToPoints(elements: elements) else {
-            XCTFail("Failed initialzied GesturableGraph")
+        let width: Double = Double(elements.count - 1) * 100
+        let height: Double = (elements.max()! - elements.min()!) * 100
+        guard var graph = graphForTest(elements: elements) else {
+            XCTFail("Failed initialzied graph")
             return
         }
 
-        gesturableGraph.paddingOfBottom(scale: 0.5)
+        graph.padding.bottom = 0.5
 
-        let result = gesturableGraph.convertToPoints(elements)
-        let expected = [(0, 50), (100, 0), (200, 200)]
-            .map { CGPoint(x: $0.0, y: $0.1) }
+        let result = graph.points
+        let expected: [Graph.Point] = [(0, 50), (100, 0), (200, 200)]
+            .map { (x: $0.0 / width, y: $0.1 / height) }
 
-        XCTAssertEqual(result, expected)
+        for (r, e) in zip(result, expected) {
+            XCTAssertEqual(r.x, e.x)
+            XCTAssertEqual(r.y, e.y)
+        }
     }
 }
